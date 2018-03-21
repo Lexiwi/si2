661cab49697af9ea27e05916b8efabffdcdf21cc
Manuel <manujs@outlook.com>
2018-03-21 15:43:30 +0100

    carpeta P2 con las apps que vamos a usar, script para limpiar y replegar las 4 apps y editado build.xml de p1-ws para que pille el comando ant replegar y desplegar 💩💩 💩

---
+++ p2/P2/auto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+:'
+Este script se encarga de limpiar y replegar todas nuestras aplicaciones
+'
+
+for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
+	cd $i
+	ant replegar; ant delete-pool-local
+	cd -
+done
+
+cd P1-base
+
+ant delete-db
+cd -
+
+
+for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
+	cd $i
+	ant limpiar-todo todo
+	cd -
+done
